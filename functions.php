5a2ce4f1882af654edfa2a01ad693f70325ed7e2
Matt Davey <matt.davey12@gmail.com>
2018-09-06 13:39:18 -0500

    Created custom post type.

--- functions.php
+++ functions.php
@@ -87,5 +87,50 @@ function theme_remove_version() {
 }
 add_filter('the_generator', 'theme_remove_version');
 
+/*
+    ================================
+    Custom Post Type
+    ================================
+*/
+function custom_post_type() {
+
+    $labels = array(
+        'name' => 'Portfolio',
+        'singular_name' => 'Portfolio',
+        'add_new' => 'Add Item',
+        'add_new_item' => 'Add Item',
+        'edit_item' => 'Edit Item',
+        'new_item' => 'New Item',
+        'view_item' => 'View Item',
+        'search_item' => 'Search Portfolio',
+        'not_found' => 'No items found',
+        'not_found_in_trash' => 'No items found in trash',
+        'parent_item_colon' => 'Parent Item'
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive'=> true,
+        'publicly_queryable' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'heirarchical' => false,
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'thumbnail',
+            'revision'
+        ),
+        'taxonomies' => array('category', 'post_tag'),
+        'menu_position' => 5,
+        'exclude_from_search' => false
+    );
+    register_post_type('portfolio', $args);    
+
+}
+add_action('init', 'custom_post_type');
+
 
 ?>
---
+++ single-portfolio.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+<section class="page-header page-header-xs dark">
+	<div class="container">
+
+		<h1><?php wp_title(''); ?></h1>
+		
+	</div>
+</section>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8">
+                <?php
+                    if (have_posts()):
+
+                        while (have_posts()): the_post();?>
+
+                        <h3><?php the_title();?> </h3>
+                        <?php if (has_post_thumbnail() ) : ?>
+                            <div class="float-right"><?php the_post_thumbnail('thumbnail'); ?></div>
+                        <?php endif; ?>
+                        <small><?php the_category(' '); ?> || <?php the_tags(); ?> || <?php edit_post_link(); ?></small>
+
+                        <p><?php the_content();?></p>
+                        
+                        <hr />
+                        <div class="row">
+                            <div class="col-md-6 text-left">
+                                <?php previous_post_link(); ?>
+                            </div>
+                            <div class="col-md-6 text-right">
+                                <?php next_post_link(); ?>
+                            </div>                            
+                        </div>
+                                                                   
+
+                        <?php 
+                        endwhile;
+                    endif;
+                ?>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <?php get_sidebar();?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
