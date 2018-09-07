1503bba920f1c7eb7015adae565afdfbf8d8b245
Matt Davey <matt.davey12@gmail.com>
2018-09-07 16:00:03 -0500

    Added hierarchical taxonomy for custom post type.

--- functions.php
+++ functions.php
@@ -123,7 +123,7 @@ function custom_post_type() {
             'thumbnail',
             'revision'
         ),
-        'taxonomies' => array('category', 'post_tag'),
+        //'taxonomies' => array('category', 'post_tag'),
         'menu_position' => 5,
         'exclude_from_search' => false
     );
@@ -131,6 +131,39 @@ function custom_post_type() {
 
 }
 add_action('init', 'custom_post_type');
+
+function custom_taxonomies() {
+
+    // add new taxonomy hierarchical
+    $labels = array(
+        'name' => 'Fields',
+        'singular_name' => 'Field',
+        'search_items' => 'Search Fields',
+        'all_items' => 'All Fields',
+        'parent_item' => 'Parent Field',
+        'parent_item_colon' => 'Parent Field:',
+        'edit_item' => 'Edit Field',
+        'update_item' => 'Update Field',
+        'add_new_item' => 'Add New Field',
+        'new_item_name' => 'New Field Name',
+        'menu_name' => 'Field'
+    );
+
+    $args = array(
+        'hierarchical' => true,        
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'field' )
+    );
+
+    register_taxonomy('field', array('portfolio'), $args);
+
+    // add new taxonomy NOT hierarchical
+    
+}
+add_action('init', 'custom_taxonomies');
 
 
 ?>
